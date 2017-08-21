9b11863915e7b64e91c8828b0177988a6c93639c
Nichelle Hall <nichellehall@Nichelles-MacBook-Pro.local>
2017-08-21 17:15:58 -0500

    rewrote Auth file

--- Auth.rb
+++ Auth.rb
@@ -11,72 +11,41 @@ SLACK_CONFIG = {
   slack_redirect_uri:ENV['redirect_url']
 }
 
-# Set the OAuth scope of your bot. We're just using `bot` for this demo, as it has access to
-# all the things we'll need to access. See: https://api.slack.com/docs/oauth-scopes for more info.
 BOT_SCOPE = 'bot'
 
-# This hash will contain all the info for each authed team, as well as each team's Slack client object.
-# In a production environment, you may want to move some of this into a real data store.
-$teams = {}
 
 class Auth < Sinatra::Base
-
-add_to_slack_button = %(
-    <a href=\"https://slack.com/oauth/authorize?scope=#{BOT_SCOPE}&client_id=#{SLACK_CONFIG[:slack_client_id]}&redirect_uri=#{SLACK_CONFIG[:slack_redirect_uri]}\">
-      <img alt=\"Add to Slack\" height=\"40\" width=\"139\" src=\"https://platform.slack-edge.com/img/add_to_slack.png\"/>
-    </a>
+  $access_token
+  add_to_slack_button = %(
+     <a href=\"https://slack.com/oauth/authorize?scope=#{BOT_SCOPE}&client_id=#{SLACK_CONFIG[:slack_client_id]}&redirect_uri=#{SLACK_CONFIG[:slack_redirect_uri]}\">
+       <img alt=\"Add to Slack\" height=\"40\" width=\"139\" src=\"https://platform.slack-edge.com/img/add_to_slack.png\"/>
+     </a>
   )
 
   get '/' do
     redirect '/begin_auth'
   end
 
-   # OAuth Step 1: Show the "Add to Slack" button, which links to Slack's auth request page.
-  # This page shows the user what our app would like to access and what bot user we'd like to create for their team.
+
   get '/begin_auth' do
-    status 200
-    body add_to_slack_button
-    # uri = URI('http://localhost:3000/api/slackbot_endpoint')
-    # res = Net::HTTP.get(uri)
-    # data = JSON.parse(res)
-    # puts data
-    # puts res
+     status 200
+     body add_to_slack_button
   end
 
-  # OAuth Step 2: The user has told Slack that they want to authorize our app to use their account, so
-  # Slack sends us a code which we can use to request a token for the user's account.
-  get '/finish_auth' do
-    client = Slack::Web::Client.new
-    # OAuth Step 3: Success or failure
+  get 'finish_auth' do
     begin
-      response = client.oauth_access(
-        {
-          client_id: SLACK_CONFIG[:slack_client_id],
-          client_secret: SLACK_CONFIG[:slack_api_secret],
-          redirect_uri: SLACK_CONFIG[:slack_redirect_uri],
-          code: params[:code] # (This is the OAuth code mentioned above)
-        }
-      )
-      # Success:
-      # Yay! Auth succeeded! Let's store the tokens and create a Slack client to use in our Events handlers.
-      # The tokens we receive are used for accessing the Web API, but this process also creates the Team's bot user and
-      # authorizes the app to access the Team's Events.
-      team_id = response['team_id']
-      $teams[team_id] = {
-        user_access_token: response['access_token'],
-        bot_user_id: response['bot']['bot_user_id'],
-        bot_access_token: response['bot']['bot_access_token']
-      }
-
-      # $teams[team_id]['client'] = create_slack_client(response['bot']['bot_access_token'])
-      # Be sure to let the user know that auth succeeded.
+      slack_code = params[:code]
+      uri = URI.parse("https://slack.com/api/oauth.access")
+      response = Net::HTTP.post_form(uri, {"code" => "slack_code", "client_id" => SLACK_CONFIG[:slack_client_id], "client_secret" => SLACK_CONFIG[:slack_api_secret]})
+      $bot_auth_token  = response["access_token"]
       status 200
-      body "Yay! Auth succeeded! You're awesome!"
-    rescue Slack::Web::Api::Error => e
-      # Failure:
-      # D'oh! Let the user know that something went wrong and output the error message returned by the Slack client.
-      status 403
-      body "Auth failed! Reason: #{e.message}<br/>#{add_to_slack_button}"
+    rescue 
+       status 403
+       body "Auth failed."
     end
+  end
+
+  def get_token
+    return $access_token
   end
 end
